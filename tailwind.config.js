844bf89c385ccdfe07c6db6aa7d0a8232fd37d9f
uneejung <yuunvely.j@gmail.com>
2026-06-28 22:39:07 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Noto Sans KR'", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
 };
